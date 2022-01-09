37fdd9407ce24eed8c45861bf619251a5e9bff1a
Salix <saulpachecotrilles.@gmail.com>
2022-01-09 19:25:15 +0100

    Productos y busquedas acabado pequeño error con tres peticiones GET

--- Udemy/portafolio/src/app/services/productos.service.ts
+++ Udemy/portafolio/src/app/services/productos.service.ts
@@ -8,18 +8,58 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
 
   cargando = true;
-  productos: Producto[] = []
+  productos: Producto[] = [];
+  productosFiltrados: Producto[] = [];
 
   constructor( private http: HttpClient) { 
     this.cargarProductos();
   }
 
+  // No se porque no va si pongo (resp: Producto[]) en vez de (resp: any) //En la insterfaz no esta puesto que el valor pueda o no aparecer
   private cargarProductos() {
-    this.http.get('https://portafolio-angular-ebe82-default-rtdb.firebaseio.com/productos_idx.json')
+
+    return new Promise<void>(( resolve, rejects)=>{
+      this.http.get('https://portafolio-angular-ebe82-default-rtdb.firebaseio.com/productos_idx.json')
         .subscribe( (resp: any) =>{
-          console.log(resp);
           this.productos = resp;
           this.cargando = false;
+          resolve();
         });
+    });
+
+    
+  }
+
+  getProducto(id: string){
+    return this.http.get(`https://portafolio-angular-ebe82-default-rtdb.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto( termino: string){
+    
+    if (this.productos.length === 0) {
+      //Cargar productos
+      this.cargarProductos().then(()=>{
+        //Ejecutar después de tener los productos
+        //Aplicar filtros
+        this.filtrarProductos(termino);
+      });
+    }else{
+      //Aplicar filtro
+      this.filtrarProductos(termino);
+    }
+
+  }
+
+  private filtrarProductos( termino: string){
+    this.productosFiltrados = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod =>{
+      const tituloLower = prod.titulo.toLowerCase();
+      if(prod.categoria.indexOf(termino)>=0 || tituloLower.indexOf(termino)>=0){
+        this.productosFiltrados.push(prod);
+      }
+    })
   }
 }
