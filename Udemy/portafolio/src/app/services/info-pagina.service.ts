08c431bcc232ea009493c5ad7f7790f27e14007d
Salix <saulpachecotrilles.@gmail.com>
2022-01-09 03:54:52 +0100

    implementacion firebase

--- Udemy/portafolio/src/app/services/info-pagina.service.ts
+++ Udemy/portafolio/src/app/services/info-pagina.service.ts
@@ -10,14 +10,29 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
-  constructor( private http: HttpClient) { 
-    // console.log('Servicio de infoPagina listo');
+  equipo: any[]=[];
 
+  constructor( private http: HttpClient) { 
+
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+  }
+
+  private cargarInfo() {
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina)=>{
           this.cargada = true;
           this.info = resp;
+        });
+  }
+
+  private cargarEquipo() {
+    //Leer el archivo JSON
+    this.http.get('https://portafolio-angular-ebe82-default-rtdb.firebaseio.com/equipo.json')
+        .subscribe( (resp:any) =>{
+          this.equipo = resp;
           console.log(resp);
         });
   }
